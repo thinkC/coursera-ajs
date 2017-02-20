7a8f7f88f50c33744f20cc183f36a8f331e86214
Babatunde Oyewo <tunde.oyewo@gmail.com>
2017-02-20 11:43:20 +0100

    add new app

---
+++ module3-solution/js/app.js
@@ -0,0 +1,80 @@
+(function(){
+   'use strict';
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+    .directive('foundItems', FoundItemsDirective);
+    
+    function FoundItemsDirective(){
+        var ddo = {
+            templateUrl : 'foundItem.html',
+            scope : {
+                items : '<',
+                onRemove: '&'
+            }
+        }
+        return ddo;
+    }
+    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService){
+        var narrowCtrl = this;
+        narrowCtrl.found = [];
+        narrowCtrl.search = function(searchTerm){
+            narrowCtrl.found = MenuSearchService.getMatchedMenuItems(narrowCtrl.searchTerm);
+            ;
+            
+        };
+
+        narrowCtrl.remove = function(index){
+            narrowCtrl.found.splice(index, 1);
+        }
+        
+    }
+    
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http,ApiBasePath){
+        var service = this;
+        
+        
+        service.getData = function(){
+            $http({
+                method: "GET",
+                url:(ApiBasePath + "/menu_items.json")
+            }).then(function(response){
+                console.log(response.data )
+                service.data = response.data;
+                
+                
+            },function errorCallback(response){
+                service.data= "Something went wrong";
+
+            });
+            
+            
+        };
+        
+        
+            service.getData()
+        
+        service.getMatchedMenuItems =function(searchTerm){
+              var items = service.data.menu_items;
+              console.log(items)
+              var found = [];
+              for(var i = 0; i < items.length; i++){
+                  var desc = items[i].description;
+                  
+                if(desc.indexOf(searchTerm) > - 1){
+                    found.push(items[i])
+                
+            }
+            
+        };
+              return found;
+          }
+        
+    }
+
+})();
+
